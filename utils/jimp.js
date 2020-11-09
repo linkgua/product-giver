b92d8475e79ddd47ea9e68bfe0969a3b3d7d3a8c
Elismer Bongiovanni <elismerb@gmail.com>
2020-11-09 10:26:18 -0300

    Change date time and fix images

--- utils/jimp.js
+++ utils/jimp.js
@@ -1,17 +1,25 @@
 const jimp = require('jimp')
 const connectDB = require("../lib/db");
+const errorHandler = require("../lib/errorHandler");
 const { IMAGE_URL } = process.env
 
 const saveImageOfProducts = async () => {
   const db = await connectDB();
   const products = await db.collection("products").find({}).toArray()
-  products.map(prod => {
+  const creations = products.map(async (prod) => {
     if (!prod.image) {
-      await saveImage(prod.value.originalImage, prod.value._id)
-      prod.image = `${IMAGE_URL}${prod._id}`
-      db.collection("products").findOneAndUpdate({ sku: prod.sku }, { $set: prod })
+      try {
+        await saveImage(prod.value.originalImage, prod.value._id)
+        prod.image = `${IMAGE_URL}${prod._id}`
+        db.collection("products").findOneAndUpdate({ sku: prod.sku }, { $set: prod })
+      }
+      catch (error) {
+        errorHandler(error)
+      }
     }
   })
+  await Promise.all(creations)
+  return true
 }
 
 const saveImage = async (url, id) => {
@@ -22,7 +30,6 @@ const saveImage = async (url, id) => {
       const image = await jimp.read(imageURL);
       await image.cover(175, 200).writeAsync(path);
     } catch (error) {
-      console.error(error);
       return error
     }
   }
